f4853b0eb12fe0e4c633f666f6d970fa2ce9cb8a
cvlecture <study.solidoss@daum.net>
2020-06-05 06:49:54 -0700

    first commit

---
+++ cvlecture/opencv_webapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+from .forms import UploadImageForm
+from django.core.files.storage import FileSystemStorage
+from .forms import ImageUploadForm
+from django.conf import settings
+
+
+def first_view(request):
+    return render(request, 'opencv_webapp/first_view.html', {})
+
+def uimage(request):
+    if request.method == 'POST':
+        form = UploadImageForm(request.POST, request.FILES)
+        if form.is_valid():
+           myfile = request.FILES['image']
+           fs = FileSystemStorage()
+           filename = fs.save(myfile.name, myfile)
+           uploaded_file_url = fs.url(filename)
+           return render(request, 'opencv_webapp/uimage.html',
+                      {'form': form, 'uploaded_file_url': uploaded_file_url})
+    else:
+        form = UploadImageForm()
+        return render(request, 'opencv_webapp/uimage.html',
+                      {'form': form})
+
+def dface(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+
+            imageURL = settings.MEDIA_URL + form.instance.document.name
+
+            return render(request, 'opencv_webapp/dface.html', {'form': form, 'post' : post})
+        else:
+            form = ImageUploadForm()
+            return render(request, 'opencv_webapp/dface.html', {'form': form})
